7dfdfddbb0458250126362f78ad54ddd6175ce98
NIck <nicholasbenzinger@gmail.com>
2021-11-15 15:26:01 -0800

    WIP trying to get GIF to display
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,4 +3,28 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$;
+$(document).ready(function() {
+  $('#submit').click(function() {
+    const search = $('searchFeild').val();
+    $('searchFeild').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}&limit=25&offset=0&rating=g&lang=en`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText).data[0].images.fixed_height.url;
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      let data = response;
+      // let htmlForGif = "<img src=" + data + ">";
+      $('.showImage').attr(data);
+    }
+  });
+});
